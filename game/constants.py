6a1a70e2937535380dfcbc495bb37116cd03d118
BuildTools <unconfigured@null.spigotmc.org>
2019-11-24 10:07:07 +0100

    constants.py added

---
+++ game/constants.py
@@ -0,0 +1,21 @@
+import enum
+
+
+class Direction(enum.IntEnum):
+    """Irányokat tároló enum"""
+
+    LEFT = 0
+    RIGHT = 1
+    DOWN = 2
+    UP = 3
+
+
+class Color:
+    """Színeket tároló osztály"""
+
+    BACKGROUND = (20, 20, 20)
+    GREEN = (64, 138, 74)
+    ORANGE = (138, 127, 64)
+    WHITE = (200, 200, 200)
+    TEXT = (100, 100, 100)
+    LINE = (30, 30, 30)
